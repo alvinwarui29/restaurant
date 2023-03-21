801772b4f4f68a48e9ce3f21a544eebbc913e766
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-03-21 16:05:39 +0300

    finish navbar section successfully

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
+import MainContainer from './components/MainContainer';
+import CreateContainer from './components/CreateContainer';
 
 const app = () => {
   return (
@@ -7,7 +10,12 @@ const app = () => {
      flex flex-col bg-primary'>
       <Header/>
 
-      <main className='mt-24 p-8 w-full'>Main Container</main>
+      <main className='mt-24 p-8 w-full'>
+        <Routes>
+          <Route path='/*' element={<MainContainer/>}/>
+          <Route path='/createItem*' element={<CreateContainer/>}/>
+        </Routes>
+      </main>
      </div>
   )
 }
---
+++ src/components/MainContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MainContainer = () => {
+  return (
+    <div>MainContainers</div>
+  )
+}
+
+export default MainContainer
